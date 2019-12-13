ab041ed51cd26a284c4bf140e4387ae56d11ee38
JSChang <papachang12@gmail.com>
2019-12-13 13:06:00 +0800

    add result visulization and save model and training history part

--- ch15 MiniVGGNet/minivggnet_cifar10.py
+++ ch15 MiniVGGNet/minivggnet_cifar10.py
@@ -18,6 +18,8 @@ from keras.optimizers import SGD
 from minivggnet import miniVGGNet
 
 import matplotlib.pyplot as plt
+import numpy as np
+#import pickle
 
 # load the dataset
 (trainX, trainY), (testX, testY) = cifar10.load_data()
@@ -39,4 +41,36 @@ labelNames = ["airplane", "automobile", "bird", "cat", "deer",
 
 # initializae the model and optimizer
 model = miniVGGNet.build(32, 32, 3, 10)
+opt = SGD(learning_rate=0.01, decay = 0.01/40,
+          momentum = 0.9, nesterov=True)
+model.compile(loss="categorical_crossentropy", optimizer=opt, 
+              metrics=["accuracy"])
 
+# train the network
+H = model.fit(trainX, trainY, validation_data = (testX, testY),
+              batch_size=64, epochs=40, verbose=1)
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=64)
+print(classification_report(testY.argmax(axis=1),
+            predictions.argmax(axis=1),target_names=labelNames))
+
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 40), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 40), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 40), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, 40), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("loss/accuracy")           
+plt.legend()
+plt.show()
+
+
+## save the model and training history
+#model.save("minivggnet_cifar10_model.h5")
+#with open("minivggnet_cifar10_mode_history.pickle", "wb") as file:
+#    pickle.dump(H.history, file)
